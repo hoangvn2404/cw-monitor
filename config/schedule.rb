1277cce12c9fddbbde7857a57dc9f7f7c9e74cbd
YOUR NAME <YOUR@EMAIL.com>
2022-05-10 18:26:52 +0700

    fix scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,11 +22,7 @@
 set :environment, "development"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-# every 5.minute do
-#   command "cd /Users/hoangnguyen/Web/cw-monitor && heroku run rake cw:update_price"
-# end
-
-every '0,5,10,15,20,25,30,35,40,45,50,55 9-14 * * 1-5' do
-  command "cd /Users/hoangnguyen/Web/cw-monitor && /opt/homebrew/bin/heroku run rake cw:update_price"
-  command "cd /Users/hoangnguyen/Web/cw-monitor && rake cw:update_price"
+every '*/5 9-14 * * 1-5' do
+  command "cd /home/hoang/Web/cw-monitor && heroku run rake cw:update_price"
+  # command "cd /home/hoang/Web/cw-monitor && /home/hoang/.rbenv/shims/rake cw:update_price"
 end
